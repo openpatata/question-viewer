3aab3f1a24e6b348d9a7364983e3a6c5658c81ca
wfdd <erdbanfed@gmail.com>
2016-09-15 13:30:14 +0300

    Reverse order by `_id` after `date`

--- src/components/main.js
+++ src/components/main.js
@@ -49,30 +49,31 @@ export const Main = React.createClass({
   },
 
   refreshData({page = 0, searchScope = 'all', searchValue = null} = this.query) {
-    let params, filters = {$page: page, $limit: 20, $orderBy: {date: -1}};
-    if (!searchValue) {
-      params = {};
-    } else if (searchScope === 'all') {
-      let regex = new RegExp(searchValue, 'i');
-      params = {
-        $or: [
-          'date', 'identifier', 'text'
-        ].map(v => ({[v]: regex}))
-         .concat({by: {mp_id: db.collection('mps').find({name: {el: regex}})
-                                                  .map(mp => mp._id)}})
-      };
-    } else if (searchScope === 'by') {
-      let mps = db.collection('mps').find({name: {el: new RegExp(searchValue, 'i')}})
-                                    .map(mp => mp._id);
-      params = {[searchScope]: {mp_id: mps}};
-    } else {
-      params = {[searchScope]: new RegExp(searchValue, 'i')};
-    }
-
-    (new Promise(resolve => window.setTimeout(() => resolve([
-      db.collection('questions').count(params),
-      db.collection('questions').find(params, filters)
-    ]), 0))).then(([questionCount, questions]) => {
+    (new Promise(resolve => window.setTimeout(() => {
+      let params, filters = {$page: page, $limit: 20, $orderBy: {date: -1, _id: -1}};
+      if (!searchValue) {
+        params = {};
+      } else if (searchScope === 'all') {
+        let regex = new RegExp(searchValue, 'i');
+        params = {
+          $or: [
+            'date', 'identifier', 'text'
+          ].map(v => ({[v]: regex}))
+           .concat({by: {mp_id: db.collection('mps').find({name: {el: regex}})
+                                                    .map(mp => mp._id)}})
+        };
+      } else if (searchScope === 'by') {
+        let mps = db.collection('mps').find({name: {el: new RegExp(searchValue, 'i')}})
+                                      .map(mp => mp._id);
+        params = {[searchScope]: {mp_id: mps}};
+      } else {
+        params = {[searchScope]: new RegExp(searchValue, 'i')};
+      }
+      return resolve([
+        db.collection('questions').count(params),
+        db.collection('questions').find(params, filters)
+      ]);
+    }, 0))).then(([questionCount, questions]) => {
       this.setState({
         docTitle: `Ερωτήσεις Κυπρίων Βουλευτών${searchValue ? `: ${searchValue}` : ''}`,
         page: parseInt(page),
